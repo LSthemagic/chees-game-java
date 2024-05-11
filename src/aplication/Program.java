b7a1a0d69613224e2af00f545476e2ec6bd63328
Railan Santana <lansilvah14fsa@gmail.com>
2024-05-11 18:38:00 -0300

    class: position

---
+++ src/aplication/Program.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import boardGame.Position;
+
+/**
+ *
+ * @author Railan Santana
+ * Data: 11 de mai. de 2024
+ */
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(0, 0);
+        System.out.println(pos);
+    }
+}
