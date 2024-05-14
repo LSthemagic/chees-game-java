a5b259d322539370d24f0ba91e5f46013e6f2a78
Railan Santana <lansilvah14fsa@gmail.com>
2024-05-14 11:16:02 -0300

    feat: chessPosition and chessException

---
+++ src/chess/ChessException.java
@@ -0,0 +1,13 @@
+package chess;
+
+/**
+ *
+ * @author Railan Santana Data: 14 de mai. de 2024
+ */
+public class ChessException extends RuntimeException{
+
+    public ChessException(String message) {
+        super(message);
+    }
+    
+}
