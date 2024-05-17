2f2014ece8587cf32e6eca0c5414593997f6a0ed
Railan Santana <lansilvah14fsa@gmail.com>
2024-05-17 18:12:20 -0300

    feat: implementation logic check

--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -60,6 +60,10 @@ public class UI {
         System.out.println();
         System.out.println("turn: " + chessMatch.getTurn());
         System.out.println("Waiting " + chessMatch.getCurrentPlayer().name().toLowerCase() + " player... ");
+        if (chessMatch.getCheck()) {
+            System.out.println("check!");
+        }
+
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
